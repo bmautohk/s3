1bfc6889ebd6737f3837e57545151780055592f6
Kelly <finalrain@gmail.com>
2015-02-11 22:53:34 +0800

    Read local jquery.js

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -3,11 +3,11 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Ben Ben 3</title>
-	<link type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet" />
+	<link type="text/css" href="/media/css/jquery-ui.css" rel="stylesheet" />
 	<? echo HTML::style("media/css/css.css"); ?>
 	<? echo HTML::style("media/css/header.css"); ?>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script src="/media/js/jquery.min.js"></script>
+	<script src="/media/js/jquery-ui.min.js"></script>
 </head>
 <body style="background-color:rgb(238, 250, 252)" leftmargin="0" topmargin="0" marginheight="0" marginwidth="0">
 <table cellspacing="0" cellpadding="0" width="100%" border="0">
